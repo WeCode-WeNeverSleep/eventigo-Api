0afbadb2b9d40db9a605e6cb379dabdd017176a1
Manda Tiavina <solontianamandanirina@gmail.com>
2026-05-02 07:17:31 +0300

    feat(server): mount public routes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import authRoutes from './routes/admin.routes.js';
+import publicRoutes from "./routes/public.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use(publicRoutes);
 
 app.get("/", (req, res) => {
     res.json({
