7d6fd25a249d6f28b2ecdaf73048a45033cfc6db
jessyrand <henintsoarandriam@gmail.com>
2026-05-08 14:45:53 +0300

    feat: update admin.route for post /rooms and post /events

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { loginController } from "../controllers/auth.controller.js";
 import { createSessionHandler } from "../controllers/session.controller.js";
 import { authenticateAdmin } from "../middlewares/auth.middleware.js";
+import { createEventController } from "../controllers/event.controller.js";
+import { createRoomController } from "../controllers/room.controller.js";
 
 const router = Router();
 
@@ -9,6 +11,8 @@ router.post("/login", loginController);
 
 router.use(authenticateAdmin);
 
+router.post("/events", createEventController);
+router.post("/rooms", createRoomController);
 router.post("/events/:eventId/sessions", createSessionHandler);
 
 export default router;
