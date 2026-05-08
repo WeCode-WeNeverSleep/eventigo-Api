3a40d2b863fc5a6cb0cd697d9166830f0fdccaae
jessyrand <henintsoarandriam@gmail.com>
2026-05-08 13:43:58 +0300

    feat(Events): add createEvent to event.service

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,4 +1,6 @@
+import type { Prisma } from "@prisma/client";
 import prisma from "../lib/prisma.js";
+import type { CreateEventInput } from "../schemas/event.schema.js";
 
 export class EventService {
   static async getEvents() {
@@ -42,6 +44,45 @@ export class EventService {
           },
         },
       },
+    });
+  }
+
+  static async createEvent(
+    organizerId: string,
+    data: CreateEventInput
+  ) {
+    const {
+      title,
+      description,
+      startDate,
+      endDate,
+      location,
+    } = data;
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    if (end <= start) {
+      throw new Error("Invalid date range");
+    }
+
+    const eventData: Prisma.EventCreateInput = {
+      title,
+      startDate: start,
+      endDate: end,
+      location,
+
+      ...(description !== undefined && { description }),
+
+      organizer: {
+        connect: {
+          id: organizerId,
+        },
+      },
+    };
+
+    return await prisma.event.create({
+      data: eventData,
     });
   }
 }
