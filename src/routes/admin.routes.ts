3d19f7642993b1798b7d8c43bcc1828121030812
jessyrand <henintsoarandriam@gmail.com>
2026-06-01 07:21:55 +0300

    feat(rooms): add admin read and update endpoints

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -3,7 +3,7 @@ import { loginController } from "../controllers/auth.controller.js";
 import { createSessionHandler } from "../controllers/session.controller.js";
 import { authenticateAdmin } from "../middlewares/auth.middleware.js";
 import { createEventController } from "../controllers/event.controller.js";
-import { createRoomController } from "../controllers/room.controller.js";
+import { createRoomController, getRoomByIdController, getRoomsController, updateRoomController } from "../controllers/room.controller.js";
 import { createSpeakerController } from "../controllers/speaker.controller.js";
 
 const router = Router();
@@ -13,7 +13,12 @@ router.post("/login", loginController);
 router.use(authenticateAdmin);
 
 router.post("/events", createEventController);
+
+router.get("/rooms", getRoomsController);
+router.get("/rooms/:roomId", getRoomByIdController);
 router.post("/rooms", createRoomController);
+router.put("/rooms/:roomId", updateRoomController);
+
 router.post("/events/:eventId/sessions", createSessionHandler);
 router.post("/speakers", createSpeakerController)
 
