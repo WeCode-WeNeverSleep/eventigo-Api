96b9b0e657771537fd4e84765cd9cf606d28e609
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-06-29 13:48:40 +0300

    feat: delete events

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,31 @@
 import { Router } from "express";
 import { loginController } from "../controllers/auth.controller.js";
-import { createSessionHandler, getAdminSessionByIdHandler, getSessionsByEventHandler, updateSessionHandler } from "../controllers/session.controller.js";
+import {
+  createSessionHandler,
+  getAdminSessionByIdHandler,
+  getSessionsByEventHandler,
+  updateSessionHandler,
+} from "../controllers/session.controller.js";
 import { authenticateAdmin } from "../middlewares/auth.middleware.js";
-import { createEventController, getAdminEventByIdController, getEventsController, updateEventController } from "../controllers/event.controller.js";
-import { createRoomController, getRoomByIdController, getRoomsController, updateRoomController } from "../controllers/room.controller.js";
-import { createSpeakerController, getAdminSpeakerByIdController, getAllSpeakersController, updateSpeakerController } from "../controllers/speaker.controller.js";
+import {
+  createEventController,
+  getAdminEventByIdController,
+  getEventsController,
+  updateEventController,
+  deleteEventController,
+} from "../controllers/event.controller.js";
+import {
+  createRoomController,
+  getRoomByIdController,
+  getRoomsController,
+  updateRoomController,
+} from "../controllers/room.controller.js";
+import {
+  createSpeakerController,
+  getAdminSpeakerByIdController,
+  getAllSpeakersController,
+  updateSpeakerController,
+} from "../controllers/speaker.controller.js";
 
 const router = Router();
 
@@ -16,6 +37,7 @@ router.get("/events", getEventsController);
 router.get("/events/:eventId", getAdminEventByIdController);
 router.post("/events", createEventController);
 router.put("/events/:eventId", updateEventController);
+router.delete("/events/:eventId", deleteEventController);
 
 router.get("/rooms", getRoomsController);
 router.get("/rooms/:roomId", getRoomByIdController);
@@ -27,9 +49,10 @@ router.get("/events/:eventId/sessions", getSessionsByEventHandler);
 router.get("/events/:eventId/sessions/:sessionId", getAdminSessionByIdHandler);
 router.put("/events/:eventId/sessions/:sessionId", updateSessionHandler);
 
-router.get("/speakers", getAllSpeakersController)
+router.get("/speakers", getAllSpeakersController);
 router.get("/speakers/:speakerId", getAdminSpeakerByIdController);
-router.post("/speakers", createSpeakerController)
+router.post("/speakers", createSpeakerController);
 router.put("/speakers/:speakerId", updateSpeakerController);
 
 export default router;
+
